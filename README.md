2b68f503f18fba3934ade053915a7743c4369df9
Vasyl Hryts <vasylhryts@knu.ua>
2022-06-07 18:09:24 +0300

    add product and their components

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
       This is JS course "Guitar-Shop"
 
 1. To do structure for project
-2.
+2. Some actions with catalog(add product and their components)
+3.
---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+  {
+    id: 'el1',
+    name: 'Гітара акустична Yamaha F310',
+    image: 'https://i.ibb.co/THr4dDx/yamaha.jpg',
+    price: 4499,
+  },
+  {
+    id: 'el2',
+    name: 'Електрогітара Ibanez GRX20 BKN',
+    image: 'https://i.ibb.co/3p4fNrk/ibanez.jpg',
+    price: 7623,
+  },
+  {
+    id: 'el3',
+    name: 'Гітара електроакустична Cort SFX-ME Black Satin (sfx me bks)',
+    image: 'https://i.ibb.co/rc6GRnY/cort.png',
+    price: 6105,
+  },
+]
